d532b1a13d48edf895733ea660575df612b01633
Mark Lawrence <mark488@ufl.edu>
2018-05-07 16:55:52 -0500

    testing

--- Ride Track/ViewController.swift
+++ Ride Track/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var listTableView: UITableView!
     
+    //Mark is back to leave some messages
+    
     var feedItems: NSArray = NSArray()
     var selectedPark: ParksModel = ParksModel()
     var parkID = 2
